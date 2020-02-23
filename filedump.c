2b6e6cbc47de76527fdff2160f24bf5f78d14e13
zykgnyue <zhangyk@tju.edu.cn>
2020-02-23 14:36:21 +0800

    rebulid add founction option for homework1

--- filedump.c
+++ filedump.c
@@ -2,13 +2,35 @@
 // 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define DF_LINEMAX (16)
+#define DF_BINMODE_BYTENUM (4)
+#define DF_HEXFMT "%2.2x   "
+#define DF_DECFMT "%03d  "
+#define DF_HEXDECSPC "%5s"
+//define support file dump mode
+typedef enum dumpxmode{
+    USAGE,  //help message
+    xMODE,  //up/down hex
+    XMODE,  //left/right hex
+    dMODE,  //up/down decimal
+    DMODE,  //left/right decimal
+    bMODE,  //up/down binary
+    BMODE   //L/R binary
+} FDUMPxmode;
 
-char buff[DF_LINEMAX];
-void printchar(char *buf,int maxlen);
-void printfmt(char *buf,int maxlen, char* fmt);
+// define enough buffer
+char buff[DF_LINEMAX+DF_BINMODE_BYTENUM];
+//prototype define
+void printchar(char *buf,int maxlen,FDUMPxmode dpmode);
+void printfmt(char *buf,int maxlen, char *fmt,FDUMPxmode dpmode);
 void create_datfile(char * fname);
-void printbin(char *buf,int maxlen);
+void printbin(char *buf,int maxlen,FDUMPxmode dpmode);
+void processbuf(char*buf, int len, FDUMPxmode dpxmode);
+int getLineLength(FDUMPxmode dpxmode);
+FDUMPxmode getdpmode(char *args);
+void printSpace(int cols,FDUMPxmode dpmode);
+
 char *asciiabl[]={ \
     "NUL","SOH","STX","ETX","EOT",
     "ENQ","ACK","BEL","BS","TAB",
@@ -21,22 +43,37 @@ char *asciiabl[]={ \
 int main(int argc,char * argv[])
 {
     unsigned char data;
+    FDUMPxmode dpxmode;
+    int binlen;
     int len=0;
+    int lenMAX = DF_BINMODE_BYTENUM;
      /*get arguments from command line*/
-    if(argc<2)
+    if(argc<3)
     {
         //not input filename 
-        printf("Usage: filedump filename\n");
-        printf("filename is the file that you want to dump its contents!\n");
+        dpxmode =USAGE;
+    }else{
+        dpxmode =getdpmode(argv[1]);
+    }
+
+    if(dpxmode == USAGE)
+    {
+        printf("Usage: filedump option file.dat\n");
+        printf("option: -x|-X|-d|-D|-b|-B \n");
+        printf("file.dat: the file will be dumped.\n");
+        printf("\n");
         return 1;
     }
-    
+
+    // get output bytes number
+    lenMAX= getLineLength(dpxmode);
+
     //create_datfile(argv[1]);
 
     //open file for binary read
     FILE *fp;
-    //file stream open for read binary mode
-    fp=fopen(argv[1],"rb");
+    //file stream open for read binary xmode
+    fp=fopen(argv[2],"rb");
     if(fp==NULL)
     {
         //open failed
@@ -52,19 +89,14 @@ int main(int argc,char * argv[])
         data=fgetc(fp);
 
         //read DF_LINEMAX byte of chars
-        if(len < DF_LINEMAX && (!feof(fp)))
+        if(len < lenMAX && (!feof(fp)))
         {
             buff[len]=data;
             len++;
         }else{
             //16byte of chars output            
-            printchar(buff,len);    
-            printfmt(buff,len,"%2.2x\t\t");
-            printfmt(buff,len,"%03d\t\t");
-            printfmt(buff,len,"%03o\t\t");
-            printbin(buff,len);
+            processbuf(buff,len,dpxmode);
 
-            printf("\n");
             len=0;
             //revise for lost 0x10 0x20 ...
             buff[len]=data;
@@ -78,8 +110,105 @@ int main(int argc,char * argv[])
 
 }
 
+//process read in data from FileStream
+void processbuf(char*buf, int len, FDUMPxmode dpxmode)
+{
+    
+    switch(dpxmode)
+    {
+        case xMODE: //up/down hex
+            printchar(buf,len,dpxmode); 
+            printf("\n");
+            printfmt(buf,len,"%2.2x\t",dpxmode);
+            printf("\n");
+            break;
+        case dMODE: //up/down dec
+            printchar(buf,len,dpxmode); 
+            printf("\n");
+            printfmt(buf,len,"%03d\t",dpxmode);
+            printf("\n");
+            break;
+        case bMODE: //up/down binary
+            printchar(buf,len,dpxmode); 
+            printf("\n");
+            printbin(buf,len,dpxmode);
+            printf("\n");
+            break;
+        case XMODE: // /L/R hex
+            printfmt(buf,len,DF_HEXFMT,dpxmode);
+            printf("  ");
+            printchar(buf,len,dpxmode); 
+            printf("\n");
+        break;
+        case DMODE: //L/R dec
+            printfmt(buf,len,DF_DECFMT,dpxmode);
+            printf("  ");
+            printchar(buf,len,dpxmode); 
+            printf("\n");
+            break;
+        case BMODE: //L/R bin
+            printbin(buf,len,dpxmode);
+            printf("  ");
+            printchar(buf,len,dpxmode); 
+            printf("\n");
+            break;
+        default:
+            //never come here
+            printf("Never come here!\n");
+            break;
+    }       
+    printf("\n");
+}
+
+//get output byte number 
+int getLineLength(FDUMPxmode dpxmode)
+{
+    //get read byte number 
+    int len;
+    if(dpxmode ==bMODE || dpxmode ==BMODE)
+    {
+        len=DF_BINMODE_BYTENUM;
+    }else{
+        len= DF_LINEMAX;
+    }
+    return  len;
+}
+
+//get selected mode 
+FDUMPxmode getdpmode(char *args)
+{
+    FDUMPxmode dpxmode;
+    if(strncmp(args,"-x",2)==0)
+    {   
+        dpxmode =xMODE;
+    }else if(strncmp(args,"-X",2)==0)
+    {
+        dpxmode =XMODE;
+    }else if(strncmp(args,"-d",2)==0)
+    {
+        dpxmode =dMODE;
+
+    }else if(strncmp(args,"-D",2)==0)
+    {
+        dpxmode =DMODE;
+
+    }else if(strncmp(args,"-b",2)==0)
+    {
+        dpxmode =bMODE;
+
+    }else if(strncmp(args,"-B",2)==0)
+    {
+        dpxmode =BMODE;
+
+    }else{
+        //usage
+        dpxmode = USAGE;
+    }
+    return dpxmode;
+}
+
 // print char with visible ascii
-void printchar(char *buf,int maxlen)
+void printchar(char *buf,int maxlen, FDUMPxmode dpmode)
 {
     int i;
     char tempdata;
@@ -89,33 +218,100 @@ void printchar(char *buf,int maxlen)
         tempdata=buf[i];
         if(tempdata <0)
         {
-            printf("%c\t\t",'?');
+            if(dpmode==XMODE || dpmode==DMODE||dpmode==BMODE)
+            {
+                printf("%c",'?');
+            }else if(dpmode==bMODE){
+
+                printf("%c\t\t",'?');
+            }else{
+                printf("%c\t",'?');
+
+            }
         }else if(tempdata<=0x20){
             //index must unsigned 
-            printf("%s\t\t",asciiabl[(unsigned char)tempdata]);
+            if(dpmode==XMODE || dpmode==DMODE||dpmode==BMODE)
+            {
+                //L/R mode replace with DOT|?
+                printf("%c",'?');
+            }else if(dpmode==bMODE){
+                //u/d mode replace with special named chars
+                printf("%s\t\t",asciiabl[(unsigned char)tempdata]);
+
+            }else{
+                printf("%s\t",asciiabl[(unsigned char)tempdata]);
+            }
+
         }else{
             //0x21==0x7F
-            if(tempdata!=0x7F)
+            if(dpmode==XMODE || dpmode==DMODE||dpmode==BMODE)
             {
-                printf("%c\t\t",tempdata);
-            }else
-            {
+                //L/R
                 //0x7F DELETE has no display font
-                /* code */
-                printf("%s\t\t","DEL");
+                printf("%c",(tempdata!=0x7f)?tempdata:'?');
+            }else{
+                //u/d 
+                if(tempdata!=0x7F)
+                {
+                    if(dpmode==bMODE)
+                    {
+                        printf("%c\t\t",tempdata);
+                    }
+                    else
+                    {
+                        /* code */
+                        printf("%c\t",tempdata);
+                    }
+                    
+                }else
+                {
+                    //0x7F DELETE has no display font
+                    /* code */
+                    if(dpmode==bMODE)
+                    {
+                        printf("%s\t\t","DEL");
+                    }else
+                    {
+                        /* code */
+                        printf("%s\t","DEL");
+                    }
+                    
+                }
             }
-            
         }
     }
-    printf("\n");
 
+
+}
+
+//print last space for L/R mode
+void printSpace(int cols,FDUMPxmode dpmode)
+{
+    int i;
+    if(dpmode ==BMODE)
+    {
+        for(i=0;i<cols;i++)
+        {
+            printf(" \t \t");
+        }
+
+    }else{
+        
+        for(i=0;i<cols;i++)
+        {
+            printf(DF_HEXDECSPC," ");
+        }
+
+
+    }
 }
 
 // print binary of the char 
 // char with visible ascii
-void printbin(char *buf,int maxlen)
+void printbin(char *buf,int maxlen,FDUMPxmode dpmode)
 {
     int i,j;
+    int templen=getLineLength(dpmode);
     for(i=0;i<maxlen;i++)
     {
         for(j=0;j<8;j++)
@@ -124,19 +320,37 @@ void printbin(char *buf,int maxlen)
         }
         printf("\t");
     }
+    //actural num < define max number
 
-    printf("\n");
+    if(maxlen<templen && maxlen!=0)
+    {
+        printSpace((templen-maxlen),dpmode);
+    }
+    // printf("\n");
 }
 
 // printf hex oct dec format
-void printfmt(char *buf,int maxlen, char* fmt)
+void printfmt(char *buf,int maxlen,char *fmt,FDUMPxmode dpmode)
 {
     int i;
+    int templen=getLineLength(dpmode);
+
     for(i=0;i<maxlen;i++)
     {
+        
         printf(fmt,(unsigned char)buf[i]);
+
     }
-    printf("\n");
+    if(dpmode == DMODE || dpmode==XMODE)
+    {
+        if(maxlen<templen && maxlen!=0)
+        {
+            printSpace((templen-maxlen),dpmode);
+
+        }
+    }
+    /// printf("\n");
+
 }
 
 //create test file with 0x00-0xff
